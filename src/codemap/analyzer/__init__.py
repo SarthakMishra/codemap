5c578f85c0ae6675b777cf8504cf357509a4a313
Sarthak <contact.sarthakmishra@gmail.com>
2025-04-29 09:42:43 +0000

    chore(analyzer): remove code analysis modules
    The code analysis modules were removed due to refactoring needs.

--- src/codemap/analyzer/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Code analysis modules for the CodeMap package."""
-
-from .processor import DocumentationProcessor
-from .tree_parser import CodeParser
-
-__all__ = ["CodeParser", "DocumentationProcessor"]
