4bab950540cd4bf34c34e2c877156fd6b0a36b60
Sarthak <contact.sarthakmishra@gmail.com>
2025-05-08 08:52:55 +0000

    chore: clean up utility module by removing unused imports and exports

--- src/codemap/utils/__init__.py
+++ src/codemap/utils/__init__.py
@@ -1,10 +1,1 @@
 """Utility module for CodeMap package."""
-
-from .cli_utils import console, loading_spinner
-from .docker_utils import ensure_qdrant_running
-
-__all__ = [
-	"console",
-	"ensure_qdrant_running",
-	"loading_spinner",
-]
